13dbeef6e5eabf012b9447dda5f4cd4a408ee162
Candida18 <64965389+Candida18@users.noreply.github.com>
2020-08-26 10:22:07 +0530

    Add files via upload

---
+++ 5. Boundary Fill Algorithm using 8-connected approach.cpp
@@ -0,0 +1,47 @@
+
+#include <graphics.h>
+#include <dos.h>
+#include <conio.h>
+
+
+void boundaryFill8(int x, int y, int fill_color,int boundary_color)
+{
+    if(getpixel(x, y) != boundary_color &&
+       getpixel(x, y) != fill_color)
+    {
+        putpixel(x, y, fill_color);
+        boundaryFill8(x + 1, y, fill_color, boundary_color);
+        boundaryFill8(x, y + 1, fill_color, boundary_color);
+        boundaryFill8(x - 1, y, fill_color, boundary_color);
+        boundaryFill8(x, y - 1, fill_color, boundary_color);
+        boundaryFill8(x - 1, y - 1, fill_color, boundary_color);
+        boundaryFill8(x - 1, y + 1, fill_color, boundary_color);
+        boundaryFill8(x + 1, y - 1, fill_color, boundary_color);
+        boundaryFill8(x + 1, y + 1, fill_color, boundary_color);
+    }
+}
+
+
+int main()
+{
+
+    int gd = DETECT, gm;
+
+
+    initgraph(&gd, &gm, "");
+
+
+    rectangle(50, 50, 150, 150);
+
+
+    boundaryFill8(55, 55, 12, 15);
+
+    delay(10000);
+
+    getch();
+
+
+    closegraph();
+
+    return 0;
+}
