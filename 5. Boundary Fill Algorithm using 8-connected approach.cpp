6629b33be2bf3721985848aa12aa3b6036be4c73
Candida Ruth Noronha <64965389+Candida18@users.noreply.github.com>
2020-12-25 19:25:59 +0530

    Update 5. Boundary Fill Algorithm using 8-connected approach.cpp

--- 5. Boundary Fill Algorithm using 8-connected approach.cpp
+++ 5. Boundary Fill Algorithm using 8-connected approach.cpp
@@ -1,13 +1,13 @@
+/*********************************************************************************
+TITLE: Program to implement Boundary Fill Algorithm using 8-connected approach.
+*********************************************************************************/
 
 #include <graphics.h>
 #include <dos.h>
 #include <conio.h>
-
-
 void boundaryFill8(int x, int y, int fill_color,int boundary_color)
 {
-    if(getpixel(x, y) != boundary_color &&
-       getpixel(x, y) != fill_color)
+    if((getpixel(x, y) != boundary_color )&& (getpixel(x, y) != fill_color))
     {
         putpixel(x, y, fill_color);
         boundaryFill8(x + 1, y, fill_color, boundary_color);
@@ -20,28 +20,15 @@ void boundaryFill8(int x, int y, int fill_color,int boundary_color)
         boundaryFill8(x + 1, y + 1, fill_color, boundary_color);
     }
 }
-
-
 int main()
 {
-
-    int gd = DETECT, gm;
-
-
-    initgraph(&gd, &gm, "");
-
-
+    int gd=DETECT,gm; //Detects the graphics drivers automatically
+	initgraph(&gd,&gm," "); //Initialize to graphics mode
+	
     rectangle(50, 50, 150, 150);
-
-
     boundaryFill8(55, 55, 12, 15);
-
     delay(10000);
-
     getch();
-
-
     closegraph();
-
     return 0;
 }
