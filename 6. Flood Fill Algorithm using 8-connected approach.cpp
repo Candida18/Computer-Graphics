b9be484a564d01613386ae4fe27150842ca852cd
Candida Ruth Noronha <64965389+Candida18@users.noreply.github.com>
2020-12-25 19:26:37 +0530

    Update 6. Flood Fill Algorithm using 8-connected approach.cpp

--- 6. Flood Fill Algorithm using 8-connected approach.cpp
+++ 6. Flood Fill Algorithm using 8-connected approach.cpp
@@ -1,10 +1,14 @@
+/*********************************************************************************
+TITLE: Program to implement Flood Fill Algorithm using 8-connected approach.
+*********************************************************************************/
+
 #include<graphics.h>
 #include<dos.h>
 #include<conio.h>
 void floodFill(int x,int y,int old,int newcol)
 {
-                if(getpixel(x,y)==old)
-                {
+    if(getpixel(x,y)==old)
+    {
 		putpixel(x,y,newcol);
 		floodFill(x-1,y,old,newcol);
 		floodFill(x+1,y,old,newcol);
@@ -19,12 +23,14 @@ void floodFill(int x,int y,int old,int newcol)
 
 int main()
 {
-                int gd=DETECT,gm;
-                initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
+        int gd=DETECT,gm; //Detects the graphics drivers automatically
+		initgraph(&gd,&gm," "); //Initialize to graphics mode
+
 		rectangle(50,50,150,150);
 		floodFill(70,70,0,1);
 		delay(10000);
-                getch();
-                closegraph();
-		return 0;
+
+        getch();
+        closegraph();
+        return 0;
 }
