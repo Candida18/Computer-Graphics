98d17160d1ed9efefe3d99d45ab8d217063cd87d
Candida Ruth Noronha <64965389+Candida18@users.noreply.github.com>
2020-12-25 19:25:02 +0530

    Update 4. Midpoint Ellipse Drawing Algorithm.cpp

--- 4. Midpoint Ellipse Drawing Algorithm.cpp
+++ 4. Midpoint Ellipse Drawing Algorithm.cpp
@@ -1,65 +1,82 @@
-#include<stdio.h>
-#include<conio.h>
+/*********************************************************************************
+TITLE:  Program to implement Midpoint Ellipse Algorithm.
+*********************************************************************************/
 #include<graphics.h>
+#include<stdio.h>
 #include<math.h>
-void pixel(int xc, int yc, int x, int y)
+
+//Function to plot the ellipse
+void pixel (int xc, int yc, int x,int y)
 {
-	putpixel(xc+x,yc+y,YELLOW);
-	putpixel(xc+x,yc-y,YELLOW);
-	putpixel(xc-x,yc-y,YELLOW);
-	putpixel(xc-x,yc+y,YELLOW);
+	putpixel(320+(xc+x),240-(yc+y),14);
+	putpixel(320+(xc+x),240-(yc-y),14);
+	putpixel(320+(xc-x),240-(yc-y),14);
+	putpixel(320+(xc-x),240-(yc+y),14);
 }
+
 int main()
 {
-int gdriver=DETECT,gmode,xc,yc,x,y,p,rx,ry;
+	int xc, yc, x, y, p, rx, ry;
 
-initgraph(&gdriver,&gmode,"C:\\TURBOC3\\BGI");
-printf("Enter the co-ordinates of the centre:\n");
-scanf("%d %d",&xc,&yc);
-printf("Enter the value of rx and ry of the ellipse:\n");
-scanf("%d %d",&rx,&ry);
-x=0;
-y=ry;
-p=(ry*ry)-(rx*rx*ry)+((rx*rx)/4);
-pixel(xc,yc,x,y);
+	int gdriver=DETECT, gmode; //Detects the graphics drivers automatically
+	initgraph(&gdriver, &gmode,""); //Initialize to graphics mode
 
-while((2*x*ry*ry)<(2*y*rx*rx))
-{
-if(p<0)
-{
-x=x+1;
-p=p+(2*ry*ry*x)+(ry*ry);
-}
-else
-{
-x=x+1;
-y=y-1;
-p=p+(2*ry*ry*x+ry*ry)-(2*rx*rx*y);
-}
-	pixel(xc,yc,x,y);
-}
-p=ry*ry*pow((float)x+0.5,2)+rx*rx*pow(y-1,2)-(rx*rx*ry*ry);
-pixel(xc,yc,x,y);
-while(y>=0)
-{
-if(p>0)
-{
-	y=y-1;
-	p=p-(2*rx*rx*y)+(rx*rx);
-}
-else
-{
-x=x+1;
-y=y-1;
-p=p+(2*ry*ry*x)-(2*rx*rx*y)+(rx*rx);
-}
-pixel(xc,yc,x,y);
-}
-getch();
-closegraph();
-return 0;
-}
+	printf("Enter the co-ordinates of the center of the ellipse : ");
+	scanf("%d %d",&xc,&yc);
+	printf("Enter the value of rx and ry of the ellipse : ");
+	scanf("%d %d",&rx,&ry);
 
+    //Take start position as(0,ry)
+    x=0;
+	y=ry;
 
+	p=(ry*ry)-(rx*rx*ry)+((rx*rx)/4); //Finding decision parameter p in region 1
 
+    //To plot the quadrants
+    line(0,240,639,240);
+    line(320,0,320,479);
+    outtextxy(295,243,"0,0");
+    setcolor(WHITE);
+
+	pixel (xc, yc, x, y);
+
+	while((2*x*ry*ry)<(2*y*rx*rx)) //At each x position in region 1
+	{
+		if(p<0) //If decision parameter is less than 0
+		{
+			x++; //Increment x
+			p=p+(2*ry*ry*x)+(ry*ry); //Calculate the new decision parameter
+		}
+		else //If decision parameter is greater than 0
+        {
+            x++; //Increment x
+            y--; //Decrement y
+            p=p+(2*ry*ry*x)+(ry*ry)-(2*rx*rx*y); //Calculate the new decision parameter
+        }
+        pixel (xc, yc, x, y);
+	}
+
+    p=ry*ry*pow((float)x+0.5,2) + rx*rx*pow(y-1,2)-rx*rx*ry*ry; //Finding decision parameter p in region 2
+    pixel (xc, yc, x, y);
+
+    while(y>=0) //At each y position in region 2
+    {
+		if(p>0) //If decision parameter is greater than 0
+        {
+            y--; //Decrement y
+            p=p-(2*y*rx*rx)+(rx*rx); //Calculate the new decision parameter
+        }
+        else //If decision parameter is less than 0
+        {
+            y--; //Decrement y
+            x++; //Increment x
+            p=p+(2*ry*ry*x)-(2*y*rx*rx)+(rx+rx); //Calculate the new decision parameter
+        }
+        pixel (xc, yc, x, y);
+    }
+
+    getch(); //Pauses the Output Console until a key is pressed
+    closegraph(); //Closes the graphics mode
+    return 0;
+}
 
